5a802f2025680a0ebaf376c3e34dfee2ec08add4
yahyabakkali <ybakkali>
2020-12-20 21:43:44 +0100

    map implementation

--- src/info/h417/Main.java
+++ src/info/h417/Main.java
@@ -4,6 +4,7 @@ import info.h417.Model.Stream.BaseInputStream;
 import info.h417.Model.Stream.BaseOutputStream;
 import info.h417.Model.Stream.Buffered.BufferedGenerator;
 import info.h417.Model.Stream.Generator;
+import info.h417.Model.Stream.Mmap.MmapGenerator;
 import info.h417.Model.Stream.One.OneGenerator;
 import info.h417.Model.Stream.OneBuffer.OneBufferGenerator;
 
@@ -11,21 +12,31 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Generator generator = new OneBufferGenerator(9);
+//        Generator generator = new OneBufferGenerator(9);
+//
+//        BaseInputStream baseInputStream = generator.getInputStream("File1");
+//        baseInputStream.open();
+//        baseInputStream.seek(8);
+//        System.out.println(baseInputStream.readln());
+//        System.out.println(baseInputStream.readln());
+//        baseInputStream.close();
+//
+//        BaseOutputStream outputStream = generator.getOutputStream("Test2");
+//        outputStream.create();
+//        outputStream.writeln("Bonjour tout vas bien ");
+//        outputStream.writeln("Bonjour tout vas bien ");
+//        outputStream.writeln("Bonjour tout vas bien ");
+//        outputStream.writeln("Bonjourn ");
+//        outputStream.close();
 
-        BaseInputStream baseInputStream = generator.getInputStream("File1");
+        Generator generator = new MmapGenerator(2);
+
+        BaseInputStream baseInputStream = generator.getInputStream("src/info/h417/file.txt");
         baseInputStream.open();
-        baseInputStream.seek(8);
-        System.out.println(baseInputStream.readln());
-        System.out.println(baseInputStream.readln());
+        //baseInputStream.seek(0);
+        while (!baseInputStream.readln().equals("EOS")) {
+            System.out.println(baseInputStream.readln());
+        }
         baseInputStream.close();
-
-        BaseOutputStream outputStream = generator.getOutputStream("Test2");
-        outputStream.create();
-        outputStream.writeln("Bonjour tout vas bien ");
-        outputStream.writeln("Bonjour tout vas bien ");
-        outputStream.writeln("Bonjour tout vas bien ");
-        outputStream.writeln("Bonjourn ");
-        outputStream.close();
     }
 }
--- src/info/h417/Model/Stream/Mmap/MmapInputStream.java
+++ src/info/h417/Model/Stream/Mmap/MmapInputStream.java
@@ -2,9 +2,18 @@ package info.h417.Model.Stream.Mmap;
 
 import info.h417.Model.Stream.BaseInputStream;
 
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 
 public class MmapInputStream extends BaseInputStream {
     private int nbCharacters;
+    private long pos = 0;
+    private FileChannel fileChannel;
+    private MappedByteBuffer buffer;
 
     /**
      * Constructor of an inputStream that reads by mapping and unmapping
@@ -16,11 +25,42 @@ public class MmapInputStream extends BaseInputStream {
     public MmapInputStream(String filename,int nbCharacters) {
         super(filename);
         this.nbCharacters = nbCharacters;
+        try {
+            fileChannel = FileChannel.open(Paths.get(filename), StandardOpenOption.READ);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public String readln() {
-        return null;
+    public void seek(long pos) {
+        this.pos = pos;
+    }
+
+    @Override
+    public String readln() throws IOException {
+        if (this.pos <= fileChannel.size()) {
+            StringBuilder line = new StringBuilder();
+            long n = (this.pos + nbCharacters <= fileChannel.size()) ? this.nbCharacters : fileChannel.size() - this.pos;
+
+            try {
+                buffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, this.pos, n);
+                System.out.println("-----------");
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            for (int i = 0; i < buffer.limit(); i++) {
+                line.append((char) (buffer.get() & 0xFF));
+            }
+
+            pos += nbCharacters;
+            return line.toString();
+        }
+        return "EOS";
     }
 
 }
