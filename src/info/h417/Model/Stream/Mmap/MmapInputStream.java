23af9fade6eb994f4670458222af823ceb7042fa
sajouaou <soufiane.ajouaou@ulb.ac.be>
2020-12-21 04:17:43 +0100

    fix outputStream new problem (fix outputStream new problem)

--- src/info/h417/Model/Stream/Mmap/MmapInputStream.java
+++ src/info/h417/Model/Stream/Mmap/MmapInputStream.java
@@ -2,13 +2,16 @@ package info.h417.Model.Stream.Mmap;
 
 import info.h417.Model.Stream.BaseInputStream;
 
+import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
@@ -16,8 +19,8 @@ import java.nio.file.StandardOpenOption;
 public class MmapInputStream extends BaseInputStream {
     private int nbCharacters;
     private MappedByteBuffer buffer;
-    private CharBuffer charBuffer = null;
     private FileChannel fc;
+    private RandomAccessFile r;
 
     /**
      * Constructor of an inputStream that reads by mapping and unmapping
@@ -36,7 +39,8 @@ public class MmapInputStream extends BaseInputStream {
     public void open() throws IOException {
         //super.open();
         if(fc == null){
-            fc = new RandomAccessFile(filename, "r").getChannel();
+            r = new RandomAccessFile(filename, "r");
+            fc = r.getChannel();
         }
     }
 
@@ -54,29 +58,33 @@ public class MmapInputStream extends BaseInputStream {
     public String readln() throws IOException {
         String text = "";
         boolean loop = true;
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
         while(!end_of_stream() && loop){
             long n = (fc.position() + nbCharacters <= fc.size()) ? this.nbCharacters : fc.size() - fc.position();
             buffer = fc.map(FileChannel.MapMode.READ_ONLY, fc.position(), n);
-            charBuffer = Charset.forName("UTF-8").decode(buffer);
             long newPosition = fc.position() ;
-            for (int i = 0; i < charBuffer.limit(); i++) {
-                char character = charBuffer.get();
+            for (int i = 0; i < buffer.limit(); i++) {
+                byte character = buffer.get();
                 if(character  == '\n' ||  character == '\r') {
                     loop = false;
                     newPosition += 1;
                 }
                 if(loop){
-                    text += character;
+                    output.write(character);
                     newPosition += 1;
                 }
             }
             seek(newPosition);
         }
+        text += StandardCharsets.UTF_8.decode(ByteBuffer.wrap(output.toByteArray() )).toString();
+        output.close();
+
         return text;
     }
 
     @Override
     public void close() throws IOException {
+        r.close();
         fc.close();
     }
 }
